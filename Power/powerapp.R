05b034b8cb2c12dca1b78a07cf5fa01b1ff7c10d
Emily Turvey <eturvey@calpoly.edu>
2018-08-17 09:23:59 -0500

    Adjustments made to fix 1 prop; comments added

--- Power/powerapp.R
+++ Power/powerapp.R
@@ -8,20 +8,32 @@ library(gridExtra)
 ui <- fluidPage(
   headerPanel("Power Applet"),
   sidebarPanel(
+    
+    #Start by choosing type of test
+    
     selectInput("select", label = "Sample Type", 
                 choices = list("1 Sample Proportion" = 1, "2 Sample Proportion" = 2, "1 Sample Mean" = 3, "2 Sample Mean" = 4), 
                 selected = 1),
     conditionalPanel(
       condition = "input.select == 1",
+      
+      #Initial Options if you choose 1 Samp Prop
+      
       numericInput("Hyp","Hypothesised Probability of Sucess",min =0, max = 1, value = .5),
       numericInput("n","Sample Size",min =0, value = 5),
       numericInput("reps","Number of Reps",min =0,  value = 100),
       checkboxInput("checkbox", label = "Done?", value = FALSE),
       conditionalPanel(
+        
+        #After determaining null properties, ask about alt
+        
         condition="input.checkbox == true",
         numericInput("Alt","Alternative Probability of Sucess",min =0, max = 1, value = .6),
         checkboxInput("checkbox2", label = "Done?", value = FALSE),
         conditionalPanel(
+          
+          #once alt is determained, ask about test (direction and LOS)
+          
           condition="input.checkbox2 == true",
           selectInput("side", label = "Direction", 
                       choices = list("Greater" = 1, "Less" = 2, "Two Tailed" = 3), 
@@ -40,6 +52,9 @@ ui <- fluidPage(
   
     conditionalPanel(
       condition = "input.select == 2",
+      
+      #Options for 2 sample Prop
+      
       numericInput("Hyp1","Group 1 Hypothesised Probability of Sucess",min =0, max = 1, value = .5),
       numericInput("Hyp2","Group 2 Hypothesised Probability of Sucess",min =0, max = 1, value = .5),
       numericInput("Alt","Alternative Difference in Probabilities (G1-G2)",min =0, max = 1, value = .1),
@@ -50,6 +65,9 @@ ui <- fluidPage(
     ),
   
   conditionalPanel(
+    
+    #Options for 1 Samp Mean
+    
     condition = "input.select == 3",
     numericInput("stde","Standard Deviation of Population", value = 10),
     numericInput("Hypt","Hypothesized Mean", value = 10),
@@ -65,9 +83,11 @@ ui <- fluidPage(
   mainPanel(
     conditionalPanel(
       
+      #Print this graph if it is a 1 Samp Prop, make it fill screen
+      
       condition = "input.select == 1",
-      plotOutput(outputId="plot1samp",width="100%",height=600),
-      textOutput("selected_var")
+      plotOutput(outputId="plot1samp",width="100%",height=600)
+ 
       
   
       
@@ -96,10 +116,16 @@ server <- function(input,output) {
   output$selected_var <- renderText({
     "Print this shit"
   })
-  output$plot1samp <- renderPlot({
 
+  output$plot1samp <- renderPlot({
+    
+    #Plot for 1 samp Prop
+    
+    #Create vectors the length of repetitions
     nully = rep(NA, times=input$reps)
     alty = rep(NA, times=input$reps)
+    
+    #Fill these vectors with the sum of T's in samples of T/F (with specified prob) of size n (specified)
     for (i in 1:input$reps) {
       setoffivenull = sample(c(T,F),size=input$n, replace=T, prob=c(input$Hyp,1-input$Hyp))
       setoffivealt = sample(c(T,F),size=input$n, replace=T, prob=c(input$Alt,1-input$Alt))
@@ -109,54 +135,76 @@ server <- function(input,output) {
       alty[i]=totalalt
 
     }
+    
+    #Creates a data frame with all of the values; labeled with null or alt
     dat <- data.frame(DistributionType = factor(rep(c("null","alt"), each=input$reps)), 
                       num = c(nully,alty))
+    
+    #creates a vector of the probability that that number of successes will occur
     temp = rep(NA, times = input$n)
     for (i in 1:input$n) {
       temp[i] <- sum(nully==i)/length(nully)
     }
+    
+    #adds up these probabilities until we reach a number greater than LOS (starting from the end) - if first number is above LOS use n+1
     min = 0
     backwards = input$n
-    while (min < input$los){
-      hold = min
-      min = min + temp[backwards]
-      backwards = backwards-1
+    if(temp[backwards] > input$los){
+      min = 0
+      backwards = input$n + 1
+    }else{
+      while (min < input$los){
+        holdlos = min
+        holdpos = backwards
+        min = min + temp[backwards]
+        backwards = backwards-1
+      }
+      min = holdlos
+      backwards = holdpos + 1
     }
+    
+    #add up probabilities until rejection region is reached
+    
     tempalt = rep(NA, times = input$n)
     for (i in 1:input$n) {
       tempalt[i] <- sum(alty==i)/length(alty)
     }
     current = input$n
     altmin = 0
-    while (backwards+2 <= current){
+    while (backwards <= current){
+      
       altmin = altmin + tempalt[current]
       current = current-1
     }
-    rejreg = backwards+2
-    #need to deal with the +2 case
-    colorsforoutline <- dat$num == rejreg:length(input$n)
-    
+    rejreg = backwards
+
+
+ #create three stages of graphs: 1. just the null, 2. null and alt, 3. Combined dist with info about the rejection region   
 
     p1 <- ggplot()+geom_histogram(data=subset(dat,DistributionType=="null"), fill="green",aes(x=num))+ggtitle("Null Distribution")+xlab("Number of Successes")+ylab("Count")+annotate("text", -Inf, Inf, label = paste("Mean:", mean(subset(dat,DistributionType=="null")$num)), hjust = 0, vjust = 1,fontface="bold")+
-      annotate("text",  -Inf, Inf, label = paste("SD:", round(sd(subset(dat,DistributionType=="null")$num),2)), hjust = 0, vjust = 3,fill="blue",fontface="bold")+legend("hellp")
+      annotate("text",  -Inf, Inf, label = paste("SD:", round(sd(subset(dat,DistributionType=="null")$num),2)), hjust = 0, vjust = 3,fill="blue",fontface="bold")
 
     p3 <- ggplot()+geom_histogram(data=subset(dat,DistributionType=="alt"), fill="blue",aes(x=num))+ggtitle("Alternative Distribution")+xlab("Number of Successes")+ylab("Count")+annotate("text", -Inf, Inf, label = paste("Mean:", mean(subset(dat,DistributionType=="alt")$num)), hjust = 0, vjust = 1,fontface="bold")+
       annotate("text",  -Inf, Inf, label = paste("SD:", round(sd(subset(dat,DistributionType=="alt")$num),2)), hjust = 0, vjust = 3,fill="blue",fontface="bold")
  
     filler <- ggplot()
-    p2 <- ggplot()+geom_histogram(data=dat, aes(x=num,fill=DistributionType))+scale_fill_manual(values=c("blue","green"))+
+    p2g <- ggplot()+geom_histogram(data=dat, aes(x=num,fill=DistributionType))+scale_fill_manual(values=c("blue","green"))+
       theme(legend.background = element_rect(fill="grey90",size=0.5, linetype="solid"))+ 
       ggtitle("Combined Distribution")+xlab("Number of Successes")+ylab("Count")+
       geom_vline(xintercept=rejreg, col="red")+geom_rect(aes(xmin=rejreg, xmax=Inf, ymin=0, ymax=Inf, alpha=.3),fill="red")+
       guides(alpha=FALSE)+annotate("text", -Inf, Inf, label = paste("Rejection Region: X >=", rejreg), hjust = 0, vjust = 1,col="red",fontface="bold")+
-      annotate("text",  -Inf, Inf, label = paste("Hypothesized Proportion of Reps:", hold*input$reps, "/", input$reps, "=", round(hold,2)), hjust = 0, vjust = 3,col="red",fill="blue",fontface="bold")+
+      annotate("text",  -Inf, Inf, label = paste("Hypothesized Proportion of Reps:", min*input$reps, "/", input$reps, "=", round(min,2)), hjust = 0, vjust = 3,col="red",fill="blue",fontface="bold")+
       annotate("text", -Inf, Inf, label = paste("Alternaitve Proportion of Reps:", altmin*input$reps, "/", input$reps, "=", round(altmin,2) ), hjust = 0, vjust = 5,col="red",fontface="bold")
 
+    
+  #sets up the layout of the graphs
     lay <- rbind(c(1,1,1,1,1,1,2,2,2,2,2,2),
                  c(1,1,1,1,1,1,2,2,2,2,2,2),
                  c(3,3,3,3,3,3,3,3,3,3,3,3),
                  c(3,3,3,3,3,3,3,3,3,3,3,3),
                  c(3,3,3,3,3,3,3,3,3,3,3,3))
+    
+    #puts certain graphs up if certain checkbox's are marked
     if(input$checkbox == T){
       grid.arrange(p1,filler,filler,layout_matrix=lay)
     }
@@ -164,7 +212,13 @@ server <- function(input,output) {
       grid.arrange(p1,p3,filler,layout_matrix=lay)
     }
     if(input$checkbox3 == T){
-      grid.arrange(p1,p3,p2,layout_matrix=lay)
+      if(input$side == 1){
+        grid.arrange(p1,p3,p2g,layout_matrix=lay)
+      } else if(input$side == 2){
+        grid.arrange(p1,p3,p2g,layout_matrix=lay)
+      } else if(input$side == 3){
+        grid.arrange(p1,p3,p2g,layout_matrix=lay)
+      }
     }
     if(input$checkbox == F){
       grid.arrange(filler,filler,filler,layout_matrix=lay)
